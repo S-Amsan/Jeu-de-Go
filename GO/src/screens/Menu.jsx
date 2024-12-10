90811886f7dd21a1294ae687b955899e5fa730a6
S-Amsan <165779415+S-Amsan@users.noreply.github.com>
2024-12-10 13:56:37 +0100

    Ajout de crédit (dans le Menu)^^

--- GO/src/screens/Menu.jsx
+++ GO/src/screens/Menu.jsx
@@ -7,6 +7,7 @@ const Menu = () => {
             <h1 className="title">JEU DE GO</h1>
             <Link className="button" to="NbJoueurs">Jouer</Link>
             <Link className="button" to="Regles">Règles</Link>
+            <p className="credit">Jeu codé en React par Amsan, Rayan, Oskar, et Ethan <br/> Moteur de jeu gnugo-3.8</p>
         </div>
     )
 }
