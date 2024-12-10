82dc891621d7676ce26276be5c91ffb39eb6365b
S-Amsan <165779415+S-Amsan@users.noreply.github.com>
2024-12-10 21:39:38 +0100

    redirection vers le site de Gnugo et des Github de collaborateur (Menu)

--- GO/src/screens/Menu.jsx
+++ GO/src/screens/Menu.jsx
@@ -7,7 +7,11 @@ const Menu = () => {
             <h1 className="title">JEU DE GO</h1>
             <Link className="button" to="/NbJoueurs">Jouer</Link>
             <Link className="button" to="/Regles">Règles</Link>
-            <p className="credit">Jeu codé en React par Amsan, Rayan, Oskar, et Ethan <br/> Moteur de jeu gnugo-3.8</p>
+            <p className="credit">Jeu codé en React par <a href="https://github.com/S-Amsan">Amsan</a>,
+                <a href="https://github.com/Uriklef"> Rayan</a>,
+                <a href="https://github.com/Francuz4520"> Oskar</a>,
+                et <a href="https://github.com/tanounnet"> Ethan</a>
+                <br/> Moteur de jeu <a href="https://www.gnu.org/software/gnugo/gnugo.html">gnugo-3.8</a></p>
         </div>
     )
 }
