2c559c44d5323104e60922350d91d61edb063fa9
Ethan Duquenne <duquenne.ethan@gmail.com>
2024-12-27 19:40:13 +0100

    Update FinJeu.jsx

--- GO/src/screens/FinJeu.jsx
+++ GO/src/screens/FinJeu.jsx
@@ -3,26 +3,41 @@ import {Link, useLocation} from "react-router-dom";
 const FinJeu = () => {
 
     const location = useLocation();
-    const {couleur} = location.state;
-    const {nbJoueur} = location.state;
+    const {couleur} = location.state; // la couleur du vainqueur
+    const {nbJoueur} = location.state; // Le nombre de vrai joueur
+    const {campJoueurSolo} = location.state; // Couleur du joueur (si il est solo)
+
     const MessageVictoire = () => {
-        if (nbJoueur === 1) {
+        if (nbJoueur === 2) {
+            // Si 2 joueurs
             return (
-                <h1 className="title"><strong>Joueur {couleur.toUpperCase()} <br/> vous avez perdu</strong></h1>
-            )
+                <h1 className="title">
+                    Joueur {couleur.toUpperCase()}, vous avez gagné !
+                </h1>
+            );
+        } else if (nbJoueur === 1) {
+            // Si 1 joueur
+            if (couleur === campJoueurSolo) {
+                return (
+                    <h1 className="title">
+                        Félicitations ! Joueur {couleur.toUpperCase()}, vous avez gagné !
+                    </h1>
+                );
+            } else {
+                return (
+                    <h1 className="title">
+                        Joueur {campJoueurSolo.toUpperCase()}, vous avez perdu.
+                    </h1>
+                );
+            }
         }
-        return (
-            <h1 className="title"><strong>Joueur {couleur.toUpperCase()} <br/> vous avez gagné</strong></h1>
-        )
-    }
+    };
 
     return (
-        <div className={`container fin-jeu ${couleur}`}>
+        <div className={`container fin-jeu ${nbJoueur === 2 ? couleur : campJoueurSolo}`}>
             <MessageVictoire/>
-            <div className="button-container">
-                <Link className="button" to="/NbJoueurs">Rejouer</Link>
-                <Link className="button" to="/">Quitter</Link>
-            </div>
+            <Link className="button" to="/NbJoueurs">Rejouer</Link>
+            <Link className="button" to="/">Quitter</Link>
         </div>
     )
 }
